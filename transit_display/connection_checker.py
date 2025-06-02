221b86707aea221dadebe38d8f6aa0cbabf1da00
Anton Kaevenheim <akaevenheim@gmail.com>
2025-06-02 21:30:24 +0200

    progress on wifi checker structure

--- transit_display/connection_checker.py
+++ transit_display/connection_checker.py
@@ -5,8 +5,7 @@ import time
 logger = logging.getLogger(__name__)
 
 
-def wifi_connected() -> bool:
-    conn_name = get_wifi_connection_name()
+def wifi_connected(conn_name: str) -> bool:
     try:
         out = subprocess.check_output(["nmcli", "--get-values", "connection,state", "device"])
         out = out.decode("ASCII")
@@ -14,34 +13,56 @@ def wifi_connected() -> bool:
             name, state = device.split(":", 1)
             if name == conn_name and state == "connected":
                 return True
-        logger.warning("No wifi!")
         return False
     except subprocess.CalledProcessError as e:
         logger.error(f"Wifi check using nmcli failed: {e}")
         raise
 
-def get_wifi_connection_name() -> str | None:
-    """Get the nmcli name for the machine's wifi connection, or None if no wifi connection was found."""
+
+def get_wifi_connection_name() -> str:
+    """Get the nmcli name for the machine's wifi connection. Raise RuntimeError if  no wifi connection was found."""
     try:
-        out = subprocess.check_output(["nmcli", "--get-values", "name,device,type", "con", "show", "--active"])
-        out = out.decode("ASCII")
-        for line in out.splitlines():
-            name, device, device_type = line.split(":", 2)
+        conns = subprocess.check_output(["nmcli", "--get-values", "name,device,type", "con", "show", "--active"])
+        conns = conns.decode("ASCII")
+        for conn in conns.splitlines():
+            name, device, device_type = conn.split(":", 2)
             if device.startswith("wlan") and "wireless" in device_type:
                 return name
-        raise RuntimeError('No wifi connection found using nmcli.')           
+        raise RuntimeError("No wifi connection found using nmcli.")
     except subprocess.CalledProcessError as e:
         logger.error(f"Failed to use nmcli to get wifi connection name: {e}")
         raise
 
-def reconnect_wifi():
+
+def reconnect_wifi(conn_name: str):
     pass
 
 
+def has_internet() -> bool:
+    pass
+
+
+#todo: I am checking wifi status again after I confirmed it was down, that's reduntant.
+def try_reconnect_loop(conn_name: str):
+    attempts = 0
+    retry_delay = 10
+    while not has_internet() and not wifi_connected(conn_name):
+        if attempts > 10:
+            retry_delay = 60
+        elif attempts > 20:
+            raise RuntimeError(f"Max retries ({attempts}) exceeded for wifi reconnect.")
+        logger.error("Wifi seems to be down. Trying to reconnect...")
+        reconnect_wifi(conn_name)
+        time.sleep(retry_delay)
+    else:
+        logger.info("Wifi connection reestablished.")
+
+
+# todo: this loop runs on a thread and if wifi is down should cause a hint on the GUI
 def wifi_check_loop():
-    # do first wifi check 60 seconds after startup:
-    time.sleep(60)
+    wifi_conn = get_wifi_connection_name()
+
     while True:
-        if not wifi_connected():
-            reconnect_wifi()
+        if not has_internet() and not wifi_connected(wifi_conn):
+            try_reconnect_loop(wifi_conn)
         time.sleep(30)
